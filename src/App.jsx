59065a07f840a5d7276e5c4ddd8a389eb9fe9997
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-06-25 13:15:49 +0600

    Module 20 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,76 @@
-import { useState } from "react";
+import { useReducer, useState } from "react"
 
+function App() {
+  // ---------------use useState--------
+  // const [userInfo , setUserInfo]= useState({
+  //   name:"",
+  //   email:"",
+  //   phone:""
+  // });
 
-function App(){
-  const [count, setCount]= useState(0);
+  // const updateUserInfo=(e)=>{
+  //   setUserInfo({...userInfo, [e.target.name]:e.target.value})
+  // }
+  // --------------------use useReducer------
+  const initialState ={
+    name:"",
+    email:"",
+    phone:""
+  }
+  const reducer=(state , action)=>{
+    switch(action.type){
+      case "FIELD_UPDATE":
+        return{...state,
+          [action.payload.field]: action.payload.value
+        };
 
-  const increment =()=>{
-    setCount(count+1)
-  };
+      case "RESTART":
+        return initialState;
 
-  const asynkIncrement =()=>{
-    setTimeout(()=>{
-      setCount((prevValue)=> prevValue +1)
-    }, 3000)
+      default: state
+
+    }
   }
 
-  return(
+  const [ userInfo , dispatch] = useReducer(reducer , initialState);
+
+const updateUserInfo = (e) => {
+  dispatch({
+    type: "FIELD_UPDATE",
+    payload: {
+      field: e.target.name, 
+      value: e.target.value
+    }
+  });
+};
+  const handelSubmit=(e)=>{
+    e.preventDefault()
+    console.log(userInfo)
+  }
+
+  return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={asynkIncrement}>Asunk Increment</button>
+      <form onSubmit={handelSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" value={userInfo.name} onChange={updateUserInfo}/>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={userInfo.email} onChange={updateUserInfo}/>
+        </div>
+
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" id="phone" name="phone" value={userInfo.phone} onChange={updateUserInfo}/>
+        </div>
+
+        <button onClick={()=>dispatch({type:"RESTART" })}>Restart</button>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
-
 }
 
 export default App
